5a8afe20867f5b809315cf6bbdaefbacdc2e7793
Tangrenin <quentindidier@msn.com>
2021-12-22 21:28:02 +0100

    refactor: added compilations files to gitignore

--- cmake-build-debug/CMakeFiles/untitled7.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled7.dir/DependInfo.cmake
@@ -4,31 +4,22 @@ set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
 
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/AndGate.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/AndGate.cpp.obj"
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/BinaryGate.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/BinaryGate.cpp.obj"
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/Gate.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/Gate.cpp.obj"
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/InputGate.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/InputGate.cpp.obj"
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/NandGate.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/NandGate.cpp.obj"
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/NegateGate.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/NegateGate.cpp.obj"
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/NorGate.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/NorGate.cpp.obj"
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/OrGate.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/OrGate.cpp.obj"
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/OutputGate.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/OutputGate.cpp.obj"
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/UnaryGate.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/UnaryGate.cpp.obj"
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/XorGate.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/XorGate.cpp.obj"
-  "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/main.cpp" "C:/Users/user/Documents/M1-MIC/LOA/Combinatorial-Circuits-Simulator/cmake-build-debug/CMakeFiles/untitled7.dir/main.cpp.obj"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
   )
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/AndGate.cpp" "CMakeFiles/untitled7.dir/AndGate.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/AndGate.cpp.obj.d"
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/BinaryGate.cpp" "CMakeFiles/untitled7.dir/BinaryGate.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/BinaryGate.cpp.obj.d"
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/Gate.cpp" "CMakeFiles/untitled7.dir/Gate.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/Gate.cpp.obj.d"
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/InputGate.cpp" "CMakeFiles/untitled7.dir/InputGate.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/InputGate.cpp.obj.d"
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/NandGate.cpp" "CMakeFiles/untitled7.dir/NandGate.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/NandGate.cpp.obj.d"
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/NegateGate.cpp" "CMakeFiles/untitled7.dir/NegateGate.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/NegateGate.cpp.obj.d"
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/NorGate.cpp" "CMakeFiles/untitled7.dir/NorGate.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/NorGate.cpp.obj.d"
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/OrGate.cpp" "CMakeFiles/untitled7.dir/OrGate.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/OrGate.cpp.obj.d"
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/OutputGate.cpp" "CMakeFiles/untitled7.dir/OutputGate.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/OutputGate.cpp.obj.d"
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/UnaryGate.cpp" "CMakeFiles/untitled7.dir/UnaryGate.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/UnaryGate.cpp.obj.d"
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/XorGate.cpp" "CMakeFiles/untitled7.dir/XorGate.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/XorGate.cpp.obj.d"
+  "C:/Users/quent/CLionProjects/Combinatorial-Circuits-Simulator/main.cpp" "CMakeFiles/untitled7.dir/main.cpp.obj" "gcc" "CMakeFiles/untitled7.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links.
